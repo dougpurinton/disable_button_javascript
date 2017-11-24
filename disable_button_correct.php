90bd86b6e8f5f864f73b3ad33c8ffd4ba093984e
dbpurinton <D.B.Purinton@eagle.clarion.edu>
2017-11-24 11:24:02 -0500

    Allows multiple form submissions while keeping button disable.

--- disable_button_correct.php
+++ disable_button_correct.php
@@ -5,30 +5,49 @@
 <meta charset = "UTF-8"/>
 
 <?php
-if($_SERVER['REQUEST_METHOD'] === 'POST') // if(isset($_POST['SubmitButton']))
+function postIsHiddenValue($f_hidden_value)
+{
+	if (isset($_POST))
+	{
+		if (in_array($f_hidden_value, $_POST, true))
+		{
+		return true;
+		}
+	}
+return false;
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && postIsHiddenValue("myform1_value"))
 {
 	sleep(3);
-	echo "<script type = \"text/javascript\">alert(\"Yah! You were able to display PHP!\");</script>";
+	echo "<script type = \"text/javascript\">alert(\"Yah! You were able to display myform1_value in PHP!\");</script>";
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && postIsHiddenValue("myform2_value"))
+{
+	sleep(3);
+	echo "<script type = \"text/javascript\">alert(\"Yah! You were able to display myform2_value in PHP!\");</script>";
 }
 ?>
 
 <script type = "text/javascript">
 // This function returns the element passed to it by using its ID. It's used to simply improve the efficiency of coding event handlers.
 function $(id)
-{
-    return document.getElementById(id);
-}
+{return document.getElementById(id);}
 
 // This funtion will run when the page fully loads, and without causing any errors.
 function afterAllLoadsGoGoGo()
 {
-	$('submitbutton_1').disabled = false;
+	$('submitbutton1_id').disabled = false;
+	$('submitbutton2_id').disabled = false;
 }
 
-function disableSubmit1()
+function disableSubmit(thisform)
 {
-	$('submitbutton_1').disabled = true;
-	$("myform").submit();
+	$('submitbutton1_id').disabled = true;
+	$('submitbutton2_id').disabled = true;
+	thisform.action = "<?php echo htmlentities($_SERVER['PHP_SELF']); ?>"; // this action attribute can be changed to any existing PHP file.
+	thisform.submit();
 }
 
 // This is used to make sure the correct function (onload or load) is used and appended correctly, instead of recreating it (which can cause errors).
@@ -50,8 +69,15 @@ if (window.attachEvent) {
 </div>
 </noscript>
 
-<form id="myform" method="post">
-<input type="submit" name="SubmitButton" id="submitbutton_1" value="Submit" onclick="disableSubmit1()" disabled="disabled" />
+<form id="myform1_id" method="post" onsubmit="disableSubmit(this)">
+<input type="hidden" name="hidden_form_name" value="myform1_value"/>
+<input type="submit" id="submitbutton1_id" name="submitbutton1_name" value="Submit" disabled="disabled"/>
+</form>
+<br>
+<br>
+<form id="myform2_id" method="post" onsubmit="disableSubmit(this)">
+<input type="hidden" name="hidden_form_name" value="myform2_value"/>
+<input type="submit" id="submitbutton2_id" name="submitbutton2_name" value="Submit" disabled="disabled"/>
 </form>
 
 </body>
